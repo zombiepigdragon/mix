e3fb14a96023f90aca4fd20fad02e612c2ed1041
Alex Rawson <ajzecrom@gmail.com>
2020-07-14 13:26:26 -0500

    Refactor main.rs
    This begins a much larger refactor that will restructure the full
    application in a way that allows it to actually make sense from a
    design perspective.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,45 @@
 use anyhow::{anyhow, Context, Result};
-use clap::ErrorKind;
-use mix::arguments::parse_arguments;
+use mix::action::Action;
 use mix::error::MixError;
 use mix::package::Database;
+
 use std::env;
-use std::{path::Path, process};
+use std::{
+    path::{Path, PathBuf},
+    process,
+};
+
+/// The options to use throughout the application. These should be set by arguments.
+#[derive(Debug)]
+struct Options {
+    action: Action,
+    database_path: PathBuf,
+}
+
+impl Options {
+    /// Create a new Options from the environment.
+    pub fn parse() -> Result<Options> {
+        let result = mix::arguments::parse_arguments(env::args());
+        let action = match result {
+            Ok(action) => Ok(action),
+            Err(error) => match error.kind {
+                clap::ErrorKind::MissingArgumentOrSubcommand => {
+                    println!("{}", error);
+                    process::exit(1);
+                }
+                clap::ErrorKind::HelpDisplayed | clap::ErrorKind::VersionDisplayed => {
+                    println!("{}", error);
+                    process::exit(0);
+                }
+                _ => Err(error),
+            },
+        }?;
+        Ok(Options {
+            action,
+            database_path: ".mix.db".into(),
+        })
+    }
+}
 
 /// When there is no database found, prompt to create a new database.
 fn create_new_database(path: &Path) -> Result<()> {
@@ -22,7 +57,9 @@ fn create_new_database(path: &Path) -> Result<()> {
         database
             .save(path)
             .context("Failed to save the blank database to the disk.")?;
-        println!("Blank database created. It's recommended to synchronize now.");
+        eprintln!(
+            "Blank database created. Continuing execution, but synchronizing is recommended."
+        );
     } else {
         return Err(anyhow!(
             "Not creating a new package database. Restore a backup."
@@ -31,41 +68,31 @@ fn create_new_database(path: &Path) -> Result<()> {
     Ok(())
 }
 
-fn main() -> Result<()> {
-    let action = parse_arguments(env::args_os());
-    let action = match action {
-        Ok(action) => action,
-        Err(error) => {
-            eprintln!("{}", error);
-            let result = match error.kind {
-                ErrorKind::HelpDisplayed => 0,
-                ErrorKind::VersionDisplayed => 0,
-                _ => 1,
-            };
-            process::exit(result);
-        }
-    };
-    let database_path = Path::new(".mix.db");
-    let package_list = Database::load(database_path).context("Failed to load package database");
-    match package_list {
-        Ok(mut package_list) => {
-            action.execute(&mut package_list)?;
-            package_list.save(database_path)?;
-            Ok(())
-        }
-        Err(err) => {
-            // The error is from mix.
-            if let Some(err) = err.downcast_ref::<MixError>() {
-                match err {
-                    MixError::FileNotFound(_) => create_new_database(database_path)?,
-                    _ => {
-                        eprintln!("{}", err);
-                    }
-                };
-                process::exit(1);
+/// Load the package database. This will exit the process if the package database cannot be loaded for any reason.
+fn get_package_database(database_path: &Path) -> Database {
+    match Database::load(database_path) {
+        Ok(database) => database,
+        Err(error) => match error {
+            MixError::FileNotFound(_) => {
+                if let Err(error) = create_new_database(database_path) {
+                    eprintln!("{}", error);
+                    process::exit(1)
+                }
+                Database::load(database_path).unwrap()
             }
-            // The error is not a MixError, so it shouldn't be from the mix api.
-            panic!("{}", err);
-        }
+            // The error is of an unprepared type, so we can't deal with it
+            error => unimplemented!("Unhandled error loading database: {:#?}", error),
+        },
     }
+}
+
+/// The entry point of the application.
+fn main() -> Result<()> {
+    let options = Options::parse().context("Failed to parse arguments.")?;
+    let mut database = get_package_database(&options.database_path);
+    options.action.execute(&mut database)?;
+    database
+        .save(&options.database_path)
+        .context("Failed to save database.")?;
+    Ok(())
 }
